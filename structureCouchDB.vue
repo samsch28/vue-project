6f949a97486b4753f89d85f7c0c7ab04411f6331
Samantha Schmidt <samantha.schmidt.999@gmail.com>
2024-10-10 13:28:24 +0200

    ajout du fichier couchDB

---
+++ structureCouchDB.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      datas: [],
+      databaseReference: null
+    }
+  },
+
+  methods: {
+    inc() {},
+
+    initDatabase() {
+      // lancer une initialisation de la base de données
+      // Si la connexion est ok, alors je fais un fetchdata
+      const db = new PouchDB('http://localhost:5984')
+      if (db) {
+        console.log('valid')
+        return
+      }
+      console.log('invalid')
+    },
+
+    fetchData() {
+      // récupérer tous les documents de ma collection
+    }
+  },
+
+  mounted() {
+    this.initDatabase()
+  }
+}
+</script>
+
+<template>
+  <h1>InfraDon2</h1>
+  <p>Counter: {{ datas }}</p>
+  <button type="button" role="button" @click="inc">+1</button>
+</template>
