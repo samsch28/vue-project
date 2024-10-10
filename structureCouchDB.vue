f5ccef41b560fb5184cc983f296a25eba24280fa
Samantha Schmidt <samantha.schmidt.999@gmail.com>
2024-10-10 15:24:04 +0200

    implementation de la base de donnees

--- structureCouchDB.vue
+++ structureCouchDB.vue
@@ -1,40 +1,89 @@
 <script lang="ts">
-import PouchDB from 'PouchDB'
+import { ref } from 'vue'
+import PouchDB from 'pouchdb'
+declare interface Post {
+  _id: string
+  doc: {
+    post_name: string
+    post_content: string
+    attributes: {
+      creation_date: string
+    }
+  }
+}
+
 export default {
   data() {
     return {
-      datas: [],
-      databaseReference: null
+      total: 0,
+      postsData: [] as Post[],
+      document: null as Post | null,
+      storage: null as PouchDB.Database | null
     }
   },
 
-  methods: {
-    inc() {},
+  mounted() {
+    this.initDatabase()
+    this.fetchData()
+  },
 
+  methods: {
+    putDocument(document: Post) {
+      const db = ref(this.storage).value
+      if (db) {
+        db.put(document)
+          .then(() => {
+            console.log('Add ok')
+          })
+          .catch((error: any) => {
+            console.log('Add ko', error)
+          })
+      }
+    },
+
+    fetchData() {
+      const storage = ref(this.storage)
+      const self = this
+      if (storage.value) {
+        storage.value
+          .allDocs({
+            include_docs: true,
+            attachments: true
+          })
+          .then(
+            function (result: any) {
+              console.log('fetchData success', result)
+              self.postsData = result.rows
+            }.bind(this)
+          )
+          .catch(function (error: any) {
+            console.log('fetchData error', error)
+          })
+      }
+    },
     initDatabase() {
-      // lancer une initialisation de la base de données
-      // Si la connexion est ok, alors je fais un fetchdata
       const db = new PouchDB('http://admin:aBcD1234!!@localhost:5984/demo')
       if (db) {
         console.log("Connected to collection 'demo'")
       } else {
         console.warn('Something went wrong')
       }
-    },
-
-    fetchData() {
-      // récupérer tous les documents de ma collection
+      this.storage = db
     }
-  },
-
-  mounted() {
-    this.initDatabase()
   }
 }
 </script>
 
 <template>
-  <h1>InfraDon2</h1>
-  <p>Counter: {{ datas }}</p>
-  <button type="button" role="button" @click="inc">+1</button>
+  <h1>Nombre de post: {{ postsData.length }}</h1>
+  <ul>
+    <li v-for="post in postsData" :key="post._id">
+      <div class="ucfirst">
+        {{ post.doc.post_name
+        }}<em style="font-size: x-small" v-if="post.doc.attributes?.creation_date">
+          - {{ post.doc.attributes?.creation_date }}
+        </em>
+      </div>
+    </li>
+  </ul>
 </template>
